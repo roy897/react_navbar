13ebd4bb4b5e18c409f9054850166130d9b7e7b1
roy897 <roushangupta85@gmail.com>
2022-05-17 22:44:02 +0530

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+let Navbar = () => {
+    return <div id='navbar'>
+
+        <div>
+            {/* <img src="" alt="" srcset="" /> */}
+            <h3>LOGOBLACKBERY</h3>
+        </div>
+
+        <div>
+            <h3><a href="#">Services</a></h3>
+            <h3><a href="#">Projects</a></h3>
+            <h3><a href="#">About</a></h3>
+        </div>
+
+        <div>
+            <button>Contact</button>
+        </div>
+
+    </div>
+}
+
+export default Navbar;
